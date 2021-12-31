7177a2572869cbcec4a06b6de5135864f42cfb8f
wen.xiao <wen.xiao@gemli.cc>
2021-12-31 15:20:17 +0800

    del more code

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: xiaoWen
+ * @Date: 2021-12-31 15:05:26
+ * @LastEditors: xiaoWen
+ * @LastEditTime: 2021-12-31 15:13:15
+ */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
